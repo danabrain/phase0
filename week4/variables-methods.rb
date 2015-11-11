a564c9acdedd64b45800e366791b34e3d0f8f74a
danabrain <dana.breen@gmail.com>
2015-11-10 23:35:44 -0500

    Update variables-methods.rb

---
+++ week4/variables-methods.rb
@@ -0,0 +1,49 @@
+puts "Hi!  What's your first name?"
+first_name = gets.chomp
+puts "Sweet!  What's your middle name?"
+middle_name = gets.chomp
+puts "Great.  And your last name?"
+last_name = gets.chomp
+
+puts "Hello, " + first_name + " " + middle_name + " " + last_name + "!"
+
+
+puts "Heya!  What's your favorite number?"
+favorite_number = gets.chomp
+better_number = favorite_number.to_i + 1
+puts favorite_number.to_s + " is a good number, but don't you think " + better_number.to_s + " is even better??"
+
+#Challenges:
+
+https://github.com/danabrain/phase-0/blob/master/week-4/address/my_solution.rb
+
+https://github.com/danabrain/phase-0/blob/master/week-4/math/my_solution.rb
+
+#How do you define a local variable?
+
+# You set the variable name equal to a value.
+
+#How do you define a method?
+
+#You use the keyword def, then name the method, fill in parameters and the body, and end it.
+
+#What is the difference between a local variable and a method?
+
+#A local variable is a value assigned to variable name.  A method is a packet of code that you can execute over and over.
+
+#How do you run a ruby program from the command line?
+
+# ruby file_name.rb
+
+#How do you run an RSpec file from the command line?
+
+# rspec file_name_spec.rb
+
+#What was confusing about this material? What made sense?
+
+#I've had some exposure to Ruby so that part was easy, but I don't know much rspec and had to take my time working through that!
+
+
+
+
+
